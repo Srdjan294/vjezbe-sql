e856433ba7428a483bb366d2eeb2ab97b692de48
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-01-16 13:36:50 +0100

    taksi služba

---
+++ taksiSluzba.sql
@@ -0,0 +1,91 @@
+--7. Taksi služba 
+--Taksi tvrtka ima više vozila. Jedno vozilo vozi jedan vozaè. U jednoj vožnji vozaè može prevesti više putnika. 
+--Jedan putnik se naravno može tijekom vremena imati više vožnji.
+
+use master;
+go 
+drop database if exists taksiSluzba;
+go
+create database taksiSluzba;
+go
+use taksiSluzba;
+
+create table vozaci(
+
+	sifra int not null primary key identity(1,1),
+	ime varchar(100),
+	prezime varchar(100),
+	iban varchar(100)
+
+);
+
+create table vozila(
+	
+	sifra int not null primary key identity(1,1),
+	registracija varchar(100),
+	marka varchar(100),
+	vozac int
+
+);
+
+create table voznje(
+
+	sifra int not null primary key identity(1,1),
+	vrijeme datetime,
+	vozilo int
+
+);
+
+create table putnici(
+
+	sifra int not null primary key identity(1,1),
+	ime varchar(100),
+	prezime varchar(100),
+	brojMobitela varchar(100),
+
+);
+
+create table stavke(
+
+	voznja int,
+	putnik int
+
+);
+
+
+alter table vozila add foreign key (vozac) references vozaci(sifra);
+
+alter table voznje add foreign key (vozilo) references vozila(sifra);
+
+alter table stavke add foreign key (voznja) references voznje(sifra);
+alter table stavke add foreign key (putnik) references putnici(sifra);
+
+
+insert into vozaci(ime, prezime, iban) values
+('Marko', 'Jeliæ', 'HR5423400098135242496'),
+('Ivan', 'Mariæ', 'HR5423400098135549496'),
+('Josip', 'Josipoviæ', 'HR5943400098135242496');
+
+insert into vozila(registracija, marka, vozac) values
+('os-435-nk', 'golf 5', 1),
+('os-345-tk', 'mazda', 1),
+('os-003-ko', 'mercedes', 1);
+
+insert into putnici(ime, prezime, brojMobitela) values
+('Mirko', 'Voliæ', '0983276473'),
+('Jasna', 'Tabak', '0917488375'),
+('Marko', 'Toliæ', '0923748576');
+
+insert into voznje(vrijeme, vozilo) values
+('2024-12-01 17:45:00', 1),
+('2024-12-01 17:55:00', 2),
+('2024-12-01 18:45:00', 3);
+
+insert into stavke(voznja, putnik) values
+(1,1),(1,2),(1,3);
+
+select * from vozaci;
+select * from vozila;
+select * from putnici;
+select * from voznje;
+select * from stavke;
